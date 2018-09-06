7bfbd561a34850785c34ec34abb4802e307d6e93
Terrell <terrell.greene95@gmail.com>
2018-09-05 23:29:46 -0500

    Will redirect to /my-profile once logged in

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -14,13 +14,13 @@ class Login extends Component {
 
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/bookshelf");
+      this.props.history.push("/my-profile");
     }
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/bookshelf");
+      this.props.history.push("/my-profile");
     }
 
     if (prevProps.errors !== this.props.errors) {
