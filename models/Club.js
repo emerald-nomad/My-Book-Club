25ee56dd430a4079ab76366989fa97d36276cbb1
Terrell <terrell.greene95@gmail.com>
2018-10-11 17:23:56 -0500

    Moved Post model into Club model

--- models/Club.js
+++ models/Club.js
@@ -46,8 +46,51 @@ const ClubSchema = new Schema({
   },
   discussionPosts: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "posts"
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      likes: [
+        {
+          user: {
+            type: Schema.Types.ObjectId,
+            ref: "users"
+          }
+        }
+      ],
+      comments: [
+        {
+          user: {
+            type: Schema.Types.ObjectId,
+            ref: "users"
+          },
+          text: {
+            type: String,
+            required: true
+          },
+          name: {
+            type: String
+          },
+          avatar: {
+            type: String
+          },
+          date: {
+            type: Date,
+            default: Date.now
+          }
+        }
+      ],
+      date: {
+        type: Date,
+        default: Date.now
+      }
     }
   ]
 });
