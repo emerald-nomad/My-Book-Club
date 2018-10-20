0897f94e62d23c4be73c3ddcaa28e789bb8aaaaa
Terrell <terrell.greene95@gmail.com>
2018-10-20 13:08:45 -0500

    Getting reading for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 // Load routes
 const user = require("./routes/user");
@@ -38,6 +39,16 @@ app.use("/api/user", user);
 app.use("/api/profile", profile);
 app.use("/api/club", club);
 app.use("/api/club/post", posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
